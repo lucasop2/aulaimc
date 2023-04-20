f60b64a06e78ff2c78c58348a04a2a73bf9b0eaa
lucasop2 <d2021022676@unifei.edu.br>
2023-04-20 19:03:44 -0300

    inicio do projeto

---
+++ codigo.js
@@ -0,0 +1,28 @@
+console.log("teste");
+
+function calculaimc() {
+
+let peso, altura, resp , clasificaçao;
+
+peso = parseFloat(document.getElementById("edtpeso").value) ;
+altura = parseFloat(document.getElementById("edtaltura").value) ;
+
+resp = peso / (altura * altura);
+
+if (resp <= 18,5){
+clasificaçao= "abaixo do peso";
+}
+else if ( resp <= 24,9){
+
+clasificaçao= "peso ideal";
+}
+ else if ( 29.9){
+clasificaçao= "levemente acima do peso ";
+ }
+ else {
+
+clasificaçao="progamador termine a tabela";
+ }
+
+ document.getElementById("resp").innerText ="o seu imc e: " + resp + "a sua classificaçao e : "+ clasificaçao;
+}
